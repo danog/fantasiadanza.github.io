6b4841a6af359d81cd3a64f7e4278e9cec0e731e
Daniil Gentili <daniil.gentili.dg@gmail.com>
2015-08-06 22:24:49 +0200

    Update website

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://s1.fantasiadanzarovigo.com/mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
