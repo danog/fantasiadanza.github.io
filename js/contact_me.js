676ae8b13eb92194d5d3ca077add3a363e4e5f64
danogentili <daniil.gentili.dg@gmail.com>
2016-08-11 16:18:14 +0200

    Update

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,5 +1,5 @@
 $(function() {
-
+    var forms = ["corsobaby", "corsobabyfantasy", "corsofantasy", "corsokidmom", "danzaclassica", "corsofun", "corsomoderno", "zumba", "step", "fitnessmusicale", "fitnessorientale", "pilates"]
     $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
@@ -69,9 +69,91 @@ $(function() {
                 },
             })
         },
+
+
         filter: function() {
             return $(this).is(":visible");
         },
+    });
+    $.each(forms, function( index, value ) {
+        $("#contact"+value+" input,#contact"+value+" textarea").jqBootstrapValidation({
+            preventSubmit: true,
+            submitError: function($form, event, errors) {
+                // additional error messages or events
+            },
+            submitSuccess: function($form, event) {
+                // Prevent spam click and default submit behaviour
+                $("#btnSubmit"+value).attr("disabled", true);
+                event.preventDefault();
+                
+                // get values from FORM
+                var name = $("input#name"+value).val();
+                var email = $("input#email"+value).val();
+                var phone = $("input#phone"+value).val();
+                var message = $("textarea#message"+value).val();
+                var domain = "6";
+                var fromform = value;
+                var firstName = name; // For Success/Failure Message
+                // Check for white space in name for Success/Fail message
+                if (firstName.indexOf(' ') >= 0) {
+                    firstName = name.split(' ').slice(0, -1).join(' ');
+                }
+                $.ajax({
+                    url: "https://mail.daniil.it/",
+                    type: "POST",
+                    data: {
+                        name: name,
+                        phone: phone,
+                        email: email,
+                        message: message,
+                        fromform: fromform,
+                        domain: domain
+                    },
+                    cache: false,
+                    success: function(data) {
+                        $("#btnSubmit"+value).attr("disabled", false);
+                        if(data == "ok") {
+                            // Success message
+                            $('#success'+value).html("<div class='alert alert-success'>");
+                            $('#success'+value+' > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                                .append("</button>");
+                            $('#success'+value+' > .alert-success')
+                                .append("<strong>Il messaggio &egrave; stato inviato. </strong>");
+                            $('#success'+value+' > .alert-success')
+                                .append('</div>');
+                            //clear all fields
+                            $('#contact'+value).trigger("reset");
+                        } else {
+                            // Fail message
+                            $('#success'+value).html("<div class='alert alert-danger'>");
+                            $('#success'+value+' > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                                .append("</button>");
+                            $('#success'+value+' > .alert-danger').append("<strong>Scusa " + firstName + ", sembra che ci sia stato un errore. Prego riprova pi&ugrave; tardi!</strong>");
+                            $('#success'+value+' > .alert-danger').append('</div>');
+                            //clear all fields
+                            $('#contact'+value).trigger("reset");
+                        }
+                    },
+                    error: function() {
+                        $("#btnSubmit"+value).attr("disabled", false);
+                        // Fail message
+                        $('#success'+value).html("<div class='alert alert-danger'>");
+                        $('#success'+value+' > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success'+value+' > .alert-danger').append("<strong>Scusa " + firstName + ", sembra che ci sia stato un errore. Prego riprova pi&ugrave; tardi!</strong>");
+                        $('#success'+value+' > .alert-danger').append('</div>');
+                        //clear all fields
+                        $('#contact'+value).trigger("reset");
+                    },
+                })
+            },
+
+
+            filter: function() {
+                return $(this).is(":visible");
+            },
+        });
+
     });
 
     $("a[data-toggle=\"tab\"]").click(function(e) {
